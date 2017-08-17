c2d4551112912039607b8a67a81338b8f2fe4b28
loginov_is <ilyalogi2014@gmail.com>
2017-08-17 03:51:07 -0700

    Fix live edit switcher click handler

--- media/js/frontend.js
+++ media/js/frontend.js
@@ -3,9 +3,11 @@ $(function(){
         var $this = $(element);
         $this.append('<a href="'+$this.data('edit')+'" class="easyiicms-goedit" style="width: '+$this.width()+'px; height: '+$this.height()+'px;" target="_blank"></a>');
     });
-    $('#easyii-navbar input').switcher({copy: {en: {yes: '', no: ''}}}).on('change', function(){
-        var checkbox = $(this);
-        checkbox.switcher('setDisabled', true);
-        location.href = checkbox.attr('data-link') + '/' + (checkbox.is(':checked') ? 1 : 0);
-    });;
+    $('#easyii-navbar input').switcher({copy: {en: {yes: '', no: ''}}});
+    $('#easyii-navbar .switcher').on('click', function () {
+       var checkbox = $(this).find('input');
+       checkbox.switcher('setDisabled', true);
+       var checked = checkbox.is(':checked') ? 1 : 0;
+       location.href = checkbox.attr('data-link') + checked + '/';
+    });
 });
